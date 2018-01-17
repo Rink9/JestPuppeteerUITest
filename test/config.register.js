f1344113bf3619bbdaf9afb4227d24f502bb8a22
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2018-01-17 17:16:36 +0700

    add register test

---
+++ test/config.register.js
@@ -0,0 +1,13 @@
+module.exports = {
+    timeout: 20000,
+    url: "https://www.lazada.co.th/customer/account/create/",
+    ids: {
+        form: "#form-account-create",
+        male: "#RegistrationForm_gender_0",
+        email: "#RegistrationForm_email",
+        name: "#RegistrationForm_first_name",
+        password: "#RegistrationForm_password",
+        confirmPassword: "#RegistrationForm_password2",
+        submit: "#send"
+    }
+};
