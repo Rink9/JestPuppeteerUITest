9bc5d34ab24b8f6be5580d6ae73241ca4b06b010
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2018-01-17 17:01:43 +0700

    Add jest and jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const setupTestFrameworkScriptFile = path.resolve(__dirname, 'setup.js');
+
+module.exports = {
+    setupTestFrameworkScriptFile
+};
---
+++ prepare.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const screenshotDir = path.resolve(__dirname, 'screenshots');
+
+if (!fs.existsSync(screenshotDir)) {
+    fs.mkdirSync(screenshotDir);
+}
