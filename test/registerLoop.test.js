08a3ff543be3416d23948d0f15de9fcc854b4431
Mahasak Pijittum <mahasak@gmail.com>
2018-01-18 11:26:56 +0700

    Delete registerLoop.test.js

--- test/registerLoop.test.js
+++
@@ -1,9 +0,0 @@
-// const testcases = require('./register.testcase.js') 
-
-// describe('Register Loops', () => {
-//     testcases.forEach((test) => {
-//          it('...', () => {
-//              expect(...).toBe(...)
-//          })
-//     })
-// })
